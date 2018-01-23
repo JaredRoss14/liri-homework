075989dd3f21c426ff39a7cb760efcca22055adc
Jared Ross <JaredARoss14@gmail.com>
2018-01-23 16:45:47 -0500

    clean up code

--- liri.js
+++ liri.js
@@ -18,17 +18,17 @@ var twiterPrompt = inquirer.createPromptModule();
 inquirer.prompt([
 
 	{
-  	type: "list",
-  	message: "What would you like to do?",
-  	choices: ["Look up tweets by username", "Learn song information", "Learn movie details", "Manual Override"],
-  	name: "command"
- 	}
+		type: "list",
+		message: "What would you like to do?",
+		choices: ["Look up tweets by username", "Learn song information", "Learn movie details", "Manual Override"],
+		name: "command"
+	}
 
-//Asks user for information to provide tweets by username
-]).then(function(choice) {
- 	if (choice.command === "Look up tweets by username") {
+	//Asks user for information to provide tweets by username
+]).then(function (choice) {
+	if (choice.command === "Look up tweets by username") {
 		inquirer.prompt([
-
+			
 			{
 				type: "input",
 				message: "What twitter account are you looking for?",
@@ -42,103 +42,103 @@ inquirer.prompt([
 				name: "tweetQuantity",
 			},
 
-		//Takes user information to pull a certain number of tweets from a user		
-		]).then(function(inquirerResponse) {
-	 			var params = {screen_name: inquirerResponse.twitterAccount, count: parseInt(inquirerResponse.tweetQuantity)};
-				client.get('statuses/user_timeline', params, function(error, response) {
-					if (!error) {
+			//Takes user information to pull a certain number of tweets from a user		
+		]).then(function (inquirerResponse) {
+			var params = { screen_name: inquirerResponse.twitterAccount, count: parseInt(inquirerResponse.tweetQuantity) };
+			client.get('statuses/user_timeline', params, function (error, response) {
+				if (!error) {
 
 					//Loops through response length to provide tweets 
-						for (var i = 0; i < response.length; i++) {
-	          	var time = response[i].created_at;
-	          	var tweets = response[i].text;
-	          	var user = params.screen_name;
-		          console.log("");
-		          console.log("");
-		          console.log("");
-		          console.log("----------------" + time + "--------------------------");
-		          console.log("");
-		          console.log("@" + user + " tweeted:");
-		          console.log("");
-		          console.log(tweets);
-		          console.log("");
-		          console.log("");
-		          console.log("");
-		        };
-		      } 
+					for (var i = 0; i < response.length; i++) {
+						var time = response[i].created_at;
+						var tweets = response[i].text;
+						var user = params.screen_name;
+						console.log("");
+						console.log("");
+						console.log("");
+						console.log("----------------" + time + "--------------------------");
+						console.log("");
+						console.log("@" + user + " tweeted:");
+						console.log("");
+						console.log(tweets);
+						console.log("");
+						console.log("");
+						console.log("");
+					};
+				}
 
-		      //If error - log error
-		      else {
-		      	console.log(error);
-	     	  };
-	  	  });
-	  });
+				//If error - log error
+				else {
+					console.log(error);
+				};
+			});
+		});
 
-	//Prompts user for song name
+		//Prompts user for song name
 	} else if (choice.command === "Learn song information") {
 		inquirer.prompt([
 
-		{
-			type: "input",
-			message: "What song would you like information about?",
-			name: "songName",
-		}
+			{
+				type: "input",
+				message: "What song would you like information about?",
+				name: "songName",
+			}
 
-		//Takes user input and searches for track information
-		]).then(function(inquirerResponse) {
-			spotify.search({type: 'track', query: inquirerResponse.songName, limit: 1})
-			.then(function(response) {
+			//Takes user input and searches for track information
+		]).then(function (inquirerResponse) {
+			spotify.search({ type: 'track', query: inquirerResponse.songName, limit: 1 })
+				.then(function (response) {
 
-				// If no error displays information
-				if (!error) {
-					var spotifyTrack = response.tracks.items[0]
-				  console.log("");
-	        console.log("");
-	        console.log("----------------" + spotifyTrack.name + "--------------------------");
-	        console.log("");
-	        console.log("Artist(s): " + spotifyTrack.artists[0].name);
-	        console.log("Album: " + spotifyTrack.album.name);
-	        console.log("Preview Link: " + spotifyTrack.external_urls.spotify);
-	        console.log("");
-	        console.log("");
-	      } 
-				
-				// If error - log error
-	      else {
-	      	console.log(error);
-	      }
-			})
+					// If no error displays information
+					if (!error) {
+						var spotifyTrack = response.tracks.items[0]
+						console.log("");
+						console.log("");
+						console.log("----------------" + spotifyTrack.name + "--------------------------");
+						console.log("");
+						console.log("Artist(s): " + spotifyTrack.artists[0].name);
+						console.log("Album: " + spotifyTrack.album.name);
+						console.log("Preview Link: " + spotifyTrack.external_urls.spotify);
+						console.log("");
+						console.log("");
+					}
+
+					// If error - log error
+					else {
+						console.log(error);
+					}
+				})
 		});
 
-	//Gather User data for OMDB movie search
+		//Gather User data for OMDB movie search
 	} else if (choice.command === "Learn movie details") {
 		inquirer.prompt([
 
-		{
-			type: "input",
-			message: "What movie would you like information about?",
-			name: "movieName",
-		}
+			{
+				type: "input",
+				message: "What movie would you like information about?",
+				name: "movieName",
+			}
 
-		//Take user data to search for movie
-		]).then(function(inquirerResponse) {
-			request('http://www.omdbapi.com/?apikey=trilogy&t=' + inquirerResponse.movieName, function(error, response, body) {
+			//Take user data to search for movie
+		]).then(function (inquirerResponse) {
+			request('http://www.omdbapi.com/?apikey=trilogy&t=' + inquirerResponse.movieName, function (error, response, body) {
 				if (!error) {
 
 					//If no error displays movie information
 					var movieObject = JSON.parse(body);
 					console.log("");
-      		console.log("");
-					console.log('body: ' + body); 
-					console.log('Title: ' + movieObject.Title); 
+					console.log("");
+					console.log('body: ' + body);
+					console.log('Title: ' + movieObject.Title);
 					console.log('Year Released: ' + movieObject.Year);
 					console.log('IMDB Rating: ' + movieObject.imdbRating);
-					console.log('Rotten Tomatoes Rating: ', movieObject.Ratings[1].Value); 
+					console.log('Rotten Tomatoes Rating: ', movieObject.Ratings[1].Value);
 					console.log('Country Produced In: ', movieObject.Country);
-					console.log('Language: ', movieObject.Language);   
-					console.log('Plot: ', movieObject.Plot); 
-					console.log('Actors: ', movieObject.Actors); 
-				} 
+					console.log('Language: ', movieObject.Language);
+					console.log('Plot: ', movieObject.Plot);
+					console.log('Actors: ', movieObject.Actors);
+				}
 
 				//If error - log error
 				else {
@@ -148,9 +148,9 @@ inquirer.prompt([
 			});
 		});
 
-	//Gather data from random.txt
+		//Gather data from random.txt
 	} else if (choice.command === "Manual Override") {
-		fs.readFile("random.txt", "utf-8", function(error, response) {
+		fs.readFile("random.txt", "utf-8", function (error, response) {
 			if (!error) {
 
 				//If no error, reads text file
